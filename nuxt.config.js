ba284ee83457c032e181958ffcc1f586711f4637
RafaelRailton <rafaelrailton@gmail.com>
2024-06-07 14:18:26 -0400

    refactor: alterando baseUrl do axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: "http://185.209.179.253:7900",
+    baseURL: "http://185.209.179.96:7900",
     // baseURL: "http://localhost:3333",
   },
   toast: {
